9cdce1d57931a1602d6acdb255e89ab7eb2004da
Davi ferreira de sena <davifsena2@gmail.com>
2022-05-04 10:36:56 -0300

    feat: implementando form aluno

--- resources/views/aluno/create.blade.php
+++ resources/views/aluno/create.blade.php
@@ -44,24 +44,63 @@
         <!-- /.card-header -->
 
         <div class="card-body">
-            <div class="form-group">
-                <x-adminlte-input name="nome_aluno" label="Nome do aluno:" placeholder="Informe o nome do aluno a ser cadastrado." required="required" enable-feedback />
+
+            <div class="card-column col-7">
+
+                <div class="form-group col-11">
+                    <x-adminlte-input name="nome_aluno" label="Nome do aluno:" placeholder="Informe o nome do aluno a ser cadastrado." required="required" enable-feedback />
+                </div>
+                <div class="form-group col-11">
+                    <x-adminlte-input name="nome_social_aluno" label="Nome social do aluno:" placeholder="Informe o nome social do aluno a ser cadastrado." enable-feedback />
+                </div>
+                <div class="form-group col-11">
+                    <x-adminlte-input name="rg" label="RG:" placeholder="Informe o rg do aluno a ser cadastrado." required="required" enable-feedback />
+                </div>
+                <div class="form-group col-11">
+                    <x-adminlte-input name="cpf" label="Cpf:" placeholder="Informe o cpf do aluno a ser cadastrado." required="required" enable-feedback />
+                </div>
+                <div class="form-group col-11">
+                    <x-adminlte-input name="email" label="Email:" placeholder="Informe o email do aluno ou responsável" required="required" enable-feedback />
+                </div>
+
+
             </div>
-            <div class="form-group">
-                <x-adminlte-input name="rg" label="RG:" placeholder="Informe o rg do aluno a ser cadastrado." required="required" enable-feedback />
+
+            <div class="column col-5">
+
+                <div class="form-group container-textarea">
+
+                    <textarea class="description" name="descricao" id="description-aluno" placeholder="Observação"></textarea>
+
+                </div>
+
+
+                <div class="form-group col-12">
+                    <x-adminlte-select name="estado_civil" label="Estado civil:" placeholder="Informe a estado civil do aluno.">
+                        <x-adminlte-options :options="['SOLTEIRO' => 'SOLTEIRO', 'CASADO' =>'CASADO', 'VIÚVO' => 'VIÚVO', 'DIVÓRCIADO' => 'DIVÓRCIADO']" disabled="0" empty-option="Selecione uma opção..." />
+                    </x-adminlte-select>
+                </div>
+
+                <div class="form-group col-12">
+                    <x-adminlte-select name="estado_civil" label="Sexo:" placeholder="Informe sexo do aluno.">
+                        <x-adminlte-options :options="['M' => 'MASCULINO', 'F' =>'FEMININO', 'N' => 'PREFIRO NÃO DIZER']" disabled="0" empty-option="Selecione uma opção..." />
+                    </x-adminlte-select>
+                </div>
+                <div class="form-group col-12 container-date">
+                    <label for="data_nascimento">Data de nascimento:</label>
+                    <input type="date" name="data_nascimento" id="data_nascimento">
+                    
+                </div>
+
             </div>
-            <div class="form-group">
-                <x-adminlte-select name="rede" label="Rede de ensino" placeholder="Informe a rede de ensino da aluno.">
-                    <x-adminlte-options :options="['MUNICIPAL' => 'MUNICIPAL', 'ESTADUAL' =>'ESTADUAL', 'FEDERAL' => 'FEDERAL', 'PRIVADA' => 'PRIVADA']" disabled="0" empty-option="Selecione uma opção..." />
-                </x-adminlte-select>
-            </div>
+
         </div>
         <!-- /.card-body -->
-        
+
     </div>
     <div class="container-button-admin">
-            <button type="submit" class="btn backgroud-primary">@lang('Adicionar')</button>
-        </div>
+        <button type="submit" class="btn backgroud-primary">@lang('Adicionar')</button>
+    </div>
 </form>
 
 <style>
@@ -70,7 +109,6 @@
         background-color: rgb(255, 255, 255);
         box-shadow: 1px 1px 6px rgb(182, 182, 182) !important;
         border-radius: 3px;
-        overflow: hidden;
     }
 
     .backgroud-primary {
@@ -78,8 +116,36 @@
         color: rgb(255, 255, 255);
     }
 
-    .card-body{
+    .card-body {
         padding: 0 1vw !important;
+        display: flex;
+    }
+
+    #description-aluno {
+        border-radius: 0.25rem;
+        padding: 1vw;
+        margin-top: 2vh;
+    }
+
+    .column {
+        margin-bottom: 4vh;
+    }
+
+    .container-textarea {
+        height: 38.5%;
+        width: 100%;
+
+    }
+
+    .description {
+        width: 99%;
+        height: 95%;
+    }
+
+    .container-date{
+        margin-top: 3vh;
+        display: flex;
+        flex-direction: column;
     }
 </style>
 
