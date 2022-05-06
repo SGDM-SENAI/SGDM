2984d566a3403266a396d74613e15385b689035b
Davi ferreira de sena <davifsena2@gmail.com>
2022-05-06 08:41:22 -0300

    feat: implementando front-end formulário

--- resources/views/aluno/create.blade.php
+++ resources/views/aluno/create.blade.php
@@ -18,9 +18,9 @@
                 <h3 class="card-title">
                     <span>@lang('Formulário de cadastro do aluno')</span>
                 </h3>
-                <!--<a href="{{ route('aluno.index') }}" class="btn-danger btn-sm">
-                <i class="fa fa-arrow-left"></i> @lang('Voltar')
-            </a>-->
+                <a href="{{ route('aluno.index') }}" class="btn-danger btn-sm">
+                    <i class="fa fa-arrow-left"></i> @lang('Sair')
+                </a>
             </div>
         </div>
         <div class="row">
@@ -38,12 +38,14 @@
                 @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
                 @endforeach
+                
             </ul>
         </div>
         @endif
         <!-- /.card-header -->
 
-        <div class="card-body">
+        <!-- Dados gerais -->
+        <div class="card-body hidden-ativo" id="form-1">
 
             <div class="card-column col-7">
 
@@ -57,7 +59,7 @@
                     <x-adminlte-input name="rg" label="RG:" placeholder="Informe o rg do aluno a ser cadastrado." required="required" enable-feedback />
                 </div>
                 <div class="form-group col-11">
-                    <x-adminlte-input name="cpf" label="Cpf:" placeholder="Informe o cpf do aluno a ser cadastrado." required="required" enable-feedback />
+                    <x-adminlte-input type="number" name="cpf" label="Cpf:" placeholder="Informe o cpf do aluno a ser cadastrado." required="required" enable-feedback />
                 </div>
                 <div class="form-group col-11">
                     <x-adminlte-input name="email" label="Email:" placeholder="Informe o email do aluno ou responsável" required="required" enable-feedback />
@@ -87,9 +89,55 @@
                     </x-adminlte-select>
                 </div>
                 <div class="form-group col-12 container-date">
-                    <label for="data_nascimento">Data de nascimento:</label>
+                    <label for="data_nascimento">@lang('Data de nascimento:')</label>
                     <input type="date" name="data_nascimento" id="data_nascimento">
-                    
+
+                </div>
+
+            </div>
+
+        </div>
+        <!-- Dados gerais -->
+
+        <!-- Dados responsaveis -->
+        <div class="card-body" id="form-2">
+
+            <div class="card-column col-7">
+
+                <div class="form-group col-11">
+                    <x-adminlte-input name="nome_pai" label="Nome do pai aluno:" placeholder="Informe o nome do pai do aluno a ser cadastrado." required="required" enable-feedback />
+                </div>
+                <div class="form-group col-11">
+                    <x-adminlte-input name="nome_mae" label="Nome da mãe do aluno:" placeholder="Informe o nome da mãe do aluno a ser cadastrado." enable-feedback />
+                </div>
+                <div class="form-group col-11">
+                    <x-adminlte-input name="rg" label="RG:" placeholder="Informe o rg do aluno a ser cadastrado." required="required" enable-feedback />
+                </div>
+                <div class="form-group col-11">
+                    <x-adminlte-input type="number" name="cpf" label="Cpf:" placeholder="Informe o cpf do aluno a ser cadastrado." required="required" enable-feedback />
+                </div>
+              
+
+
+            </div>
+
+            <div class="column col-5">
+
+                <div class="form-group col-12">
+                    <x-adminlte-select name="estado_civil" label="Estado civil:" placeholder="Informe a estado civil do aluno.">
+                        <x-adminlte-options :options="['SOLTEIRO' => 'SOLTEIRO', 'CASADO' =>'CASADO', 'VIÚVO' => 'VIÚVO', 'DIVÓRCIADO' => 'DIVÓRCIADO']" disabled="0" empty-option="Selecione uma opção..." />
+                    </x-adminlte-select>
+                </div>
+
+                <div class="form-group col-12">
+                    <x-adminlte-select name="estado_civil" label="Sexo:" placeholder="Informe sexo do aluno.">
+                        <x-adminlte-options :options="['M' => 'MASCULINO', 'F' =>'FEMININO', 'N' => 'PREFIRO NÃO DIZER']" disabled="0" empty-option="Selecione uma opção..." />
+                    </x-adminlte-select>
+                </div>
+                <div class="form-group col-12 container-date">
+                    <label for="data_nascimento">@lang('Data de nascimento:')</label>
+                    <input type="date" name="data_nascimento" id="data_nascimento">
+
                 </div>
 
             </div>
@@ -98,10 +146,32 @@
         <!-- /.card-body -->
 
     </div>
-    <div class="container-button-admin">
-        <button type="submit" class="btn backgroud-primary">@lang('Adicionar')</button>
+    <div class="container-options">
+
+        <div class="container-button-admin">
+            <button type="button" class="btn btn-back backgroud-empty">@lang('< Voltar')</button>
+        </div>
+        <!-- <div class="container-etapa">
+            <span>Etapa <div id="num-etapa">1</div>/5</span>
+        </div> -->
+        <div class="figure-etapa">
+            <div class="figure-circle circle-ativo"></div>
+            <div class="figure-circle circle-inativo"></div>
+            <div class="figure-circle circle-inativo"></div>
+            <div class="figure-circle circle-inativo"></div>
+            <div class="figure-circle circle-inativo"></div>
+        </div>
+        <div class="container-button-admin">
+            <button type="button" class="btn  backgroud-primary">@lang('Próxima etapa >')</button>
+            <button type="submit" class=" btn hidden-ativo  backgroud-primary">@lang('Finalizar cadastro')</button>
+        </div>
+
     </div>
 </form>
+
+<script>
+    
+</script>
 
 <style>
     .card-admin {
@@ -114,6 +184,11 @@
     .backgroud-primary {
         background-color: rgb(8, 103, 57);
         color: rgb(255, 255, 255);
+    }
+
+    .backgroud-empty {
+        background-color: transparent;
+        border: rgb(0, 0, 0) 1px solid;
     }
 
     .card-body {
@@ -142,11 +217,57 @@
         height: 95%;
     }
 
-    .container-date{
+    .container-date {
         margin-top: 3vh;
         display: flex;
         flex-direction: column;
     }
+
+    .container-options,
+    .container-etapa,
+    .container-etapa span,
+    .figure-etapa {
+        display: flex;
+    }
+
+    .container-options {
+        justify-content: space-between;
+        width: 100%;
+    }
+
+    .container-etapa {
+        align-items: center;
+        text-align: center;
+    }
+
+    .btn-back:hover {
+        background-color: rgb(255, 0, 0);
+        color: rgb(255, 255, 255);
+    }
+
+    .hidden-ativo {
+        display: none;
+    }
+
+    #num-etapa {
+        margin-left: 0.2vw;
+    }
+
+    .figure-circle{
+    width: 20px;
+    height: 20px;
+    border: rgb(0, 0, 0) solid 2px;
+    border-radius: 10px;
+    margin-right: 1vw;
+
+}
+
+.circle-ativo{
+    background-color: rgb(8, 103, 57);
+}
+
+.figure-etapa{
+}
 </style>
 
 @stop
